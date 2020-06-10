e0ca798077b52699a76443dfc12f6564950c6e29
Ikenna Okafor <ikennajeshuaokafor@gmail.com>
2020-06-10 19:10:15 -0400

    Updates

--- SORT/espressoTester_run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for file in /home/ikenna/IkennaWorkSpace/espressoTester/sample_images/*.jpg; do
-    imgName=$(basename -- "$file")
-    imgName="${imgName%%.*}"
-    ./espressoTester $file $PWD/sample_img_results/"$imgName"_detection.jpg
-done
