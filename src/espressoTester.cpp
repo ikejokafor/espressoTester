4e3d122646a3f450585b233e4b8bd0166b7b71c0
Ikenna Okafor <ikennajeshuaokafor@gmail.com>
2020-06-23 15:14:49 -0400

    Updates

--- src/espressoTester.cpp
+++ src/espressoTester.cpp
@@ -1,6 +1,7 @@
 #include "espressoTester.hpp"
 using namespace std;
 
+
 #ifdef CAFFE
 espresso::layerType_t getEspLayType(string layerType)
 {
@@ -608,16 +609,21 @@ int main(int argc, char **argv)
     image sized = letterbox_image(im, networkLayerInfoArr[0]->numInputRows, networkLayerInfoArr[0]->numInputCols);
     cfgInputLayer(sized, &net, networkLayerInfoArr[0], espresso::FLOAT);
     net.cfgFPGALayers(yolov3_mrgd_fm_FN);
-    net.printMemBWStats();
+    // net.printMemBWStats();
     net.setHardware(m_sysc_fpga_hndl);
     if(argc == 2)
     {
         net.Forward(argv[1]);
     }
+    else if(argc == 3)
+    {
+        net.Forward(argv[1], argv[2]);
+    }
     else
     {
         net.Forward();
     }
+    net.printExecutionStats();
     // string imgOut_FN = "predictions";
     // string cocoNames_FN = WSpath + "/darknet/data/coco.names";
     // post_yolo(&net, yolo_net, (char*)cocoNames_FN.c_str(), sized, (char*)imgOut_FN.c_str());
